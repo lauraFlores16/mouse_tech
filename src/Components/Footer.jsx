28d57d02827f3ec4da12fe7c7b172c24ec54ffc6
Derix <derix.dv19@gmail.com>
2025-04-30 09:37:12 -0400

    areglo footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,16 +1,24 @@
 import "../styles/Footer.css";
 import logo from "../assets/logo.png";
+import ubicacionIcon from "../assets/imagen/ubicacion.png";
+import correoIcon from "../assets/imagen/correo.png";
+import telefonoIcon from "../assets/imagen/contacto.png";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
+        
+        {/* Logo y derechos */}
         <div className="footer-logo">
           <img src={logo} alt="Logo Mouse Tech" />
+          <p style={{ color: "black" }}>
+            &copy; {new Date().getFullYear()} Mouse Tech. Todos los derechos reservados.
+          </p>
 
-          <p>&copy; {new Date().getFullYear()} Mouse Tech. Todos los derechos reservados.</p>
         </div>
 
+        {/* Enlaces rápidos */}
         <div className="footer-links">
           <h4>Enlaces Rápidos</h4>
           <ul>
@@ -20,12 +28,25 @@ const Footer = () => {
           </ul>
         </div>
 
+        {/* Información de contacto */}
         <div className="footer-contacto">
           <h4>Contáctanos</h4>
-          <p><i className="fas fa-map-marker-alt"></i> La Paz, Bolivia</p>
-          <p><i className="fas fa-envelope"></i> <a href="mailto:mousetechsa@gmail.com">mousetechsa@gmail.com</a></p>
-          <p><i className="fas fa-phone"></i> <a href="tel:+59173547502">+591 735-47502</a></p>
-          <p><i className="fas fa-phone"></i> <a href="tel:+59168015204">+591 680-15204</a></p>
+          <p>
+            <img src={ubicacionIcon} alt="Ubicación" className="footer-icon" />
+            La Paz, Bolivia
+          </p>
+          <p>
+            <img src={correoIcon} alt="Correo" className="footer-icon" />
+            <a href="mailto:mousetechsa@gmail.com">mousetechsa@gmail.com</a>
+          </p>
+          <p>
+            <img src={telefonoIcon} alt="Teléfono" className="footer-icon" />
+            <a href="tel:+59173547502">+591 735-47502</a>
+          </p>
+          <p>
+            <img src={telefonoIcon} alt="Teléfono" className="footer-icon" />
+            <a href="tel:+59168015204">+591 680-15204</a>
+          </p>
         </div>
       </div>
     </footer>
